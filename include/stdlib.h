f25b12b404c2f3955671a09eb1eb3faf80bc88c9
Vlad-Andrei BĂDOIU (78692) <vlad_andrei.badoiu@stud.acs.upb.ro>
2019-11-18 15:23:32 +0000

    Remove strtold_l external symbol from stdlib.h
    Signed-off-by: Vlad-Andrei Badoiu <vlad_andrei.badoiu@stud.acs.upb.ro>
    Reviewed-by: Felipe Huici <felipe.huici@neclab.eu>

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,10 +1,4 @@
 #include <xlocale.h>
-//extern long double strtold (const char*, char**);
-//extern long double strtold(char const*, char**);
-extern long double strtold (const char *__restrict, char **__restrict);
-extern long double strtold_l (const char *__restrict, char **__restrict,
-				                  locale_t);
-
 #include_next <stdlib.h>
 
 
